29ef6efce4462c0855794bb374c16ecb823b7b2c
vuvansy <vuvansy3001@gmail.com>
2024-11-22 00:56:07 +0700

    Design App Bar

--- src/components/header/app.header.tsx
+++ src/components/header/app.header.tsx
@@ -18,6 +18,7 @@ import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
 
 //styled-component
 const Search = styled("div")(({ theme }) => ({
@@ -90,18 +91,20 @@ export default function AppHeader() {
     const renderMenu = (
         <Menu
             anchorEl={anchorEl}
-            anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-            }}
+            // anchorOrigin={{
+            //     vertical: "top",
+            //     horizontal: "right",
+            // }}
             id={menuId}
             keepMounted
-            transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-            }}
+            // transformOrigin={{
+            //     vertical: "top",
+            //     horizontal: "right",
+            // }}
             open={isMenuOpen}
             onClose={handleMenuClose}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
         >
             <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
             <MenuItem onClick={handleMenuClose}>My account</MenuItem>
@@ -166,7 +169,12 @@ export default function AppHeader() {
 
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
+            <AppBar
+                position="static"
+                sx={{
+                    background: "#5A639C",
+                }}
+            >
                 <Container>
                     <Toolbar>
                         <Typography
@@ -198,17 +206,7 @@ export default function AppHeader() {
                             <span>Playlist</span>
                             <span>Likes</span>
                             <span>Upload</span>
-                            <IconButton
-                                size="large"
-                                edge="end"
-                                aria-label="account of current user"
-                                aria-controls={menuId}
-                                aria-haspopup="true"
-                                onClick={handleProfileMenuOpen}
-                                color="inherit"
-                            >
-                                <AccountCircle />
-                            </IconButton>
+                            <Avatar onClick={handleProfileMenuOpen}>ER</Avatar>
                         </Box>
                         <Box sx={{ display: { xs: "flex", md: "none" } }}>
                             <IconButton
