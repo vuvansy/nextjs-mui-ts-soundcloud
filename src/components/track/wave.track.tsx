3473e19bd0a2a969865d37f455395a6ba48d91ee
vuvansy <vuvansy3001@gmail.com>
2024-11-22 22:01:43 +0700

    Sử dụng useMemo hook

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,27 +1,9 @@
 'use client'
 
-import { useEffect, useRef ,useState, useMemo} from "react";
-import WaveSurfer from "wavesurfer.js";
+import { useEffect, useRef, useState, useMemo } from "react";
 import { useSearchParams } from 'next/navigation';
+import { useWavesurfer } from "@/utils/customHook";
 
-// WaveSurfer hook
-const useWavesurfer = (containerRef: any, options: any) => {
-    const [wavesurfer, setWavesurfer] = useState<any>(null)
-    // Initialize wavesurfer when the container mounts
-    // or any of the props change
-    useEffect(() => {
-        if (!containerRef.current) return
-        const ws = WaveSurfer.create({
-            ...options,
-            container: containerRef.current,
-        })
-        setWavesurfer(ws)
-        return () => {
-            ws.destroy()
-        }
-    }, [options, containerRef])
-    return wavesurfer
-}
 
 const WaveTrack = () => {
     const containerRef = useRef<HTMLDivElement>(null);
@@ -29,7 +11,7 @@ const WaveTrack = () => {
     const searchParams = useSearchParams()
     const fileName = searchParams.get('audio');
 
-
+    //Biến số được sử dụng với useMemo Giữ nguyên địa chỉ bộ nhớ và giá trị mỗi lần render
     const optionsMemo = useMemo(() => {
         return {
             waveColor: 'rgb(200, 0, 200)',
@@ -38,28 +20,7 @@ const WaveTrack = () => {
         }
     }, []);
 
-
-     // const options = {
-    //     waveColor: 'rgb(200, 0, 200)',
-    //     progressColor: 'rgb(100, 0, 100)',
-    //     url: `/api?audio=${fileName}`,
-    // }
-
     const wavesurfer = useWavesurfer(containerRef, optionsMemo);
-
-    // useEffect(() => {
-    //     if (containerRef.current) {
-    //         const wavesurfer = WaveSurfer.create({
-    //             container: containerRef.current,
-    //             waveColor: 'rgb(200, 0, 200)',
-    //             progressColor: 'rgb(100, 0, 100)',
-    //             url: `/api?audio=${fileName}`,
-    //         })
-    //     }
-
-    // }, [])
-
-    
 
     return (
         <div ref={containerRef}>
--- src/utils/customHook.ts
+++ src/utils/customHook.ts
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
+import WaveSurfer from "wavesurfer.js";
+import { WaveSurferOptions } from 'wavesurfer.js';
 
 //Hook trả ra giá trị là true nếu component đã render và ngược lại trả ra là false
-
 export const useHasMounted = () => {
 
     const [hasMounted, setHasMounted] = useState<boolean>(false);
@@ -11,4 +12,25 @@ export const useHasMounted = () => {
     }, []);
 
     return hasMounted;
+}
+
+// WaveSurfer hook
+export const useWavesurfer = (containerRef: React.RefObject<HTMLDivElement>,
+    options: Omit<WaveSurferOptions, 'container'>) => {
+
+    const [wavesurfer, setWavesurfer] = useState<any>(null)
+    // Initialize wavesurfer when the container mounts
+    // or any of the props change
+    useEffect(() => {
+        if (!containerRef.current) return
+        const ws = WaveSurfer.create({
+            ...options,
+            container: containerRef.current,
+        })
+        setWavesurfer(ws)
+        return () => {
+            ws.destroy()
+        }
+    }, [options, containerRef])
+    return wavesurfer
 }
