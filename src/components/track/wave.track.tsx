2facf48723c8c75cec00ce2edfffa39bacade45e
vuvansy <vuvansy3001@gmail.com>
2024-11-23 15:44:26 +0700

    Add Gradients

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -13,9 +13,30 @@ const WaveTrack = () => {
 
     //Biến số được sử dụng với useMemo Giữ nguyên địa chỉ bộ nhớ và giá trị mỗi lần render
     const optionsMemo = useMemo((): Omit<WaveSurferOptions, 'container'> => {
+
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d')!;
+        // Define the waveform gradient
+        const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35);
+        gradient.addColorStop(0, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#B1B1B1') // Bottom color
+        gradient.addColorStop(1, '#B1B1B1') // Bottom color
+        // Define the progress gradient
+        const progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
+        progressGradient.addColorStop(0, '#EE772F') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, '#EB4926') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#F6B094') // Bottom color
+        progressGradient.addColorStop(1, '#F6B094') // Bottom color
+
         return {
-            waveColor: 'rgb(200, 0, 200)',
-            progressColor: 'rgb(100, 0, 100)',
+            waveColor: gradient,
+            progressColor: progressGradient,
+            height: 150,
             barWidth: 2,
             url: `/api?audio=${fileName}`,
         }
