91860813f75e655cfa43100f95e62a259684b070
vuvansy <vuvansy3001@gmail.com>
2024-11-25 15:31:20 +0700

    Customize Session call api inside jwt callbacks

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import { AuthOptions } from 'next-auth';
 import { sendRequest } from "../../../../utils/api";
-import async from '../../../page';
+import { JWT } from "next-auth/jwt";
 
 export const authOptions: AuthOptions = {
     secret: process.env.NO_SECRET,
@@ -16,14 +16,30 @@ export const authOptions: AuthOptions = {
         // ...add more providers here
     ],
     callbacks: {
+        //Mỗi 1 lần F5 lại trang, server giải mã token nạp ngược lại session
        async jwt({ token, user, account, profile, trigger }) {
             if (trigger === "signIn" && account?.provider === "github") {
                 
+                const res = await sendRequest<IBackendRes<JWT>>({
+                    url: "http://localhost:8000/api/v1/auth/social-media",
+                    method: "POST",
+                    body: { type: "GITHUB", username: user.email },
+                })
+                if (res.data) {
+                    token.access_token = res.data?.access_token;
+                    token.refresh_token = res.data.refresh_token;
+                    token.user = res.data.user;
+                }
+
             }
             return token;
         },
         session({ session, token, user }) {
-            
+            if (token) {
+                session.access_token = token.access_token;
+                session.refresh_token = token.refresh_token;
+                session.user = token.user;
+            }
             return session;
         }
     }
