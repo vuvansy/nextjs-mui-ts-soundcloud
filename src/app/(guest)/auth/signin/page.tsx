0e290327656b34d7e3e102ce6acd772c6956e4d9
vuvansy <vuvansy3001@gmail.com>
2024-11-25 17:20:55 +0700

    Nested Layout với Route Group

---
+++ src/app/(guest)/auth/signin/page.tsx
@@ -0,0 +1,10 @@
+import AuthSignIn from "@/components/auth/auth.signin"
+const SignInPage = () => {
+    return (
+        // <AuthSignIn />
+        <div>
+            sign in page
+        </div>
+    )
+}
+export default SignInPage;
