48dcf392deedb129e1191d7a757def544eae4602
vuvansy <vuvansy3001@gmail.com>
2024-11-26 16:05:14 +0700

    Tích hợp Drag/Drop (Part 2)

--- src/components/track/steps/step1.tsx
+++ src/components/track/steps/step1.tsx
@@ -2,9 +2,43 @@
 
 import { useDropzone, FileWithPath } from 'react-dropzone';
 import './theme.css';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { useCallback } from 'react';
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+function InputFileUpload() {
+    return (
+        <Button
+            onClick={(event) => event.preventDefault()}
+            component="label"
+            variant="contained"
+            startIcon={<CloudUploadIcon />}>
+            Upload file
+            <VisuallyHiddenInput type="file" />
+        </Button>
+    );
+}
 
 const Step1 = () => {
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+    const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+        // Do something with the files
+        console.log(">>> check acceptedFiles: ", acceptedFiles)
+    }, [])
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({ onDrop });
+
     const files = acceptedFiles.map((file: FileWithPath) => (
         <li key={file.path}>
             {file.path} - {file.size} bytes
@@ -15,7 +49,9 @@ const Step1 = () => {
         <section className="container">
             <div {...getRootProps({ className: 'dropzone' })}>
                 <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
+                <InputFileUpload
+                />
+                <p>Click hoặc Drag/Drop để upload file track!</p>
             </div>
             <aside>
                 <h4>Files</h4>
