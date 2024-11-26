a4e17d5d1025d0cbc6d90c2b3acb4c6bb3189cfc
vuvansy <vuvansy3001@gmail.com>
2024-11-26 15:18:25 +0700

    Tích hợp MUI Tabs

--- src/app/(user)/track/upload/page.tsx
+++ src/app/(user)/track/upload/page.tsx
@@ -1,8 +1,11 @@
+import UploadTabs from "@/components/track/upload.tabs";
+import { Container } from "@mui/material";
+
 const UploadPage = () => {
     return (
-        <div>
-            upload page
-        </div>
+        <Container>
+            <UploadTabs />
+        </Container>
     )
 }
 export default UploadPage;
---
+++ src/components/track/upload.tabs.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import * as React from 'react';
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+const UploadTabs = () => {
+    const [value, setValue] = React.useState(0);
+    
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Box sx={{ width: '100%', border: "1px solid #ccc", mt: 5 }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    aria-label="basic tabs example"
+                >
+                    <Tab label="Item One" />
+                    <Tab label="Item Two" />
+                    <Tab label="Item Three" />
+                </Tabs>
+            </Box>
+            <CustomTabPanel value={value} index={0}>
+                Hoi Dan IT
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={1}>
+                Item Two
+            </CustomTabPanel>
+            <CustomTabPanel value={value} index={2}>
+                Item Three
+            </CustomTabPanel>
+        </Box>
+    );
+}
+export default UploadTabs;
