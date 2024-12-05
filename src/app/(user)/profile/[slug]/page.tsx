521c063abd09da080de6d0034e80c8b6fc86e3af
vuvansy <vuvansy3001@gmail.com>
2024-12-05 14:49:56 +0700

    Render List Tracks Profile

--- src/app/(user)/profile/[slug]/page.tsx
+++ src/app/(user)/profile/[slug]/page.tsx
@@ -1,16 +1,27 @@
+import ProfileTracks from "@/components/header/profile.tracks";
 import { sendRequest } from "@/utils/api";
+import { Container, Grid } from "@mui/material";
 
 const ProfilePage = async ({ params }: { params: { slug: string } }) => {
-    const tracks = await sendRequest<IBackendRes<ITrackTop[]>>({
+    const tracks = await sendRequest<IBackendRes<IModelPaginate<ITrackTop>>>({
         url: "http://localhost:8000/api/v1/tracks/users?current=1&pageSize=10",
         method: "POST",
         body: { id: params.slug }
     })
-    console.log(">>> check tracks: ", tracks)
+
+    const data = tracks?.data?.result ?? [];
     return (
-        <div>
-            profile page slug {params.slug}
-        </div>
+        <Container sx={{ my: 5 }}>
+            <Grid container spacing={5}>
+                {data.map((item: ITrackTop, index: number) => {
+                    return (
+                        <Grid item xs={12} md={6} key={index}>
+                            <ProfileTracks data={item} />
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Container>
     )
 }
 export default ProfilePage;
