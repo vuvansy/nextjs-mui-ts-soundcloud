682b3a2422167a2c90579bb5553f869a389b103d
vuvansy <vuvansy3001@gmail.com>
2024-12-05 15:48:23 +0700

    Play/Pause Track with Context (Part 1)

--- src/components/header/profile.tracks.tsx
+++ src/components/header/profile.tracks.tsx
@@ -11,6 +11,7 @@ import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import SkipNextIcon from '@mui/icons-material/SkipNext';
 import { useTrackContext } from '@/lib/track.wrapper';
+import PauseIcon from '@mui/icons-material/Pause';
 
 interface IProps {
     data: ITrackTop
@@ -37,11 +38,33 @@ const ProfileTracks = (props: IProps) => {
                     <IconButton aria-label="previous">
                         {theme.direction === 'rtl' ? <SkipNextIcon /> : <SkipPreviousIcon />}
                     </IconButton>
-                    <IconButton aria-label="play/pause"
-                        onClick={() => setCurrentTrack({ ...data, isPlaying: false })}
-                    >
-                        <PlayArrowIcon sx={{ height: 38, width: 38 }} />
-                    </IconButton>
+                    {/* Xư lý điều kiện nút Play  */}
+                    {
+                        (data._id !== currentTrack._id ||
+                            data._id === currentTrack._id && currentTrack.isPlaying === false
+                        )
+                        &&
+                        <IconButton aria-label="play/pause"
+                            onClick={(e) => {
+                                setCurrentTrack({ ...data, isPlaying: true });
+                            }}
+                        >
+                            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+                        </IconButton>
+                    }
+                    {/* Xử lý điều kiện nút Pause  */}
+                    {data._id === currentTrack._id && currentTrack.isPlaying === true
+                        &&
+                        <IconButton aria-label="play/pause"
+                            onClick={(e) => {
+                                setCurrentTrack({ ...data, isPlaying: false });
+                            }}
+                        >
+                            <PauseIcon sx={{ height: 38, width: 38 }}
+                            />
+                        </IconButton>
+                    }
+
                     <IconButton aria-label="next">
                         {theme.direction === 'rtl' ? <SkipPreviousIcon /> : <SkipNextIcon />}
                     </IconButton>
